a993671de63c11867a23f54cd056e445236d4901
moundir99 <moundirboucherab@gmail.com>
2020-10-29 17:16:59 +0100

    QR code avec affichage image

--- screens/MainMap.js
+++ screens/MainMap.js
@@ -1,43 +1,57 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, TouchableOpacity, Dimensions} from 'react-native';
 import {Button} from 'react-native-elements';
 import {createStackNavigator} from '@react-navigation/stack';
 import DropDownPicker from 'react-native-dropdown-picker';
 import Scanner from "./Scanner";
 //import Icon from 'react-native-vector-icons/Feather';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import ImageZoom from 'react-native-image-pan-zoom';
+import ImageView from "react-native-image-view";
+import {block} from "react-native-reanimated";
+import {Modal} from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
 
 export default function MainMap({navigation, route}) {
     const [floor, setFloor] = useState(1);
     const [qrData, setQrData] = useState("");
-
+    const [click, setClick] = useState(false);
+    const images = [{
+        url: '',
+        props: {
+            // Or you can set source directory.
+            source: require('../assets/premierEtage.png')
+        }
+    }]
     if (route.params?.data) {//route.params?.type
         //WE GO HERE WHEN DATA FROM QR SCAN IS AVAILABLE, then we display the appropriate floor or room
 
     }
     return (
         <View style={styles.container}>
-            <View style={styles.map}>
-                <Text>here for the map</Text>
-            </View>
+            <TouchableOpacity style={styles.map} onPress={displayImage}>
+                <Image style={styles.image} source={require('../assets/premierEtage.png')}/>
+            </TouchableOpacity>
+            <Modal visible={click} transparent={true}  >
+                <ImageViewer imageUrls={images} enableSwipeDown={true} onCancel={close}/>
+            </Modal>
             <View style={styles.dropDownPicker}>
                 <Text>Enter the floor:</Text>
-                <DropDownPicker
-                    items={[
-                        {label: '1', value: '1', icon: () => <Icon name="flag" size={18} color="#900"/>},
-                        {label: '0', value: '0', icon: () => <Icon name="flag" size={18} color="#900"/>},
-                    ]}
-                    multiple={false}
-                    defaultValue={floor.toString()}
-                    containerStyle={{height: 40}}
-                    style={{backgroundColor: '#fafafa'}}
-                    itemStyle={{
-                        justifyContent: 'flex-start'
-                    }}
-                    dropDownStyle={{backgroundColor: '#fafafa'}}
-                    onChangeItem={(item) => {
-                        setFloor(item.value)
-                    }}
+                <DropDownPicker style={styles.dropDown}
+                                items={[
+                                    {label: '1', value: '1', icon: () => <Icon name="flag" size={18} color="#900"/>},
+                                    {label: '0', value: '0', icon: () => <Icon name="flag" size={18} color="#900"/>},
+                                ]}
+                                multiple={false}
+                                defaultValue={floor.toString()}
+                                containerStyle={{height: 40}}
+                                itemStyle={{
+                                    justifyContent: 'flex-start'
+                                }}
+                                dropDownStyle={{backgroundColor: '#fafafa'}}
+                                onChangeItem={(item) => {
+                                    setFloor(item.value)
+                                }}
                 />
             </View>
             <View style={styles.scan}>
@@ -52,27 +66,50 @@ export default function MainMap({navigation, route}) {
         </View>
 
     );
+    function close(){
+        setClick(false);
+    }
+    function displayImage(){
+        setClick(true);
+    }
+    function maMap() {
+        alert('voila');
+    }
 
     function press() {
         navigation.navigate('Scanner');
     }
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 40,
     },
+    bigImage: {
+        display: "none",
+    },
     map: {
-        flex: 2
+        flex: 4,
+        overflow: 'hidden',
+    },
+    imagestyle: {
+        width: '100%',
+        height: 'undefined',
+        aspectRatio: 1,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
     },
     dropDownPicker: {
         flex: 1
     },
     scan: {
         top: 60,
-        flex: 2
-
+        flex: 1
+    },
+    dropDown: {
+        backgroundColor: '#fafafa',
     }
 
 })
